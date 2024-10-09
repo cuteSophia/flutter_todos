7de8328a368d2d791a44c81e37f9f185f9170b5c
shijing <shijing@hex-tech.net>
2024-10-09 19:09:24 +0800

    feat: 1

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_todos/app/app.dart';
 import 'package:flutter_todos/bootstrap.dart';
 import 'package:local_storage_todos_api/local_storage_todos_api.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final todosApi = LocalStorageTodosApi()
+  final todosApi = LocalStorageTodosApi(
+    plugin: await SharedPreferences.getInstance(),
+  );
 
-  bootstrap(() => const App());
+  bootstrap(todosApi: todosApi);
 }
